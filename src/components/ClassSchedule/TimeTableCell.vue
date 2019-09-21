4d0dfb1a69404e3d1bceaaedb0ef0277a4dcfa45
Sken <k18056kk@aitech.ac.jp>
2019-09-21 19:27:52 +0900

    授業登録の時必修の色を変えて表示するようにした

--- src/components/ClassSchedule/TimeTableCell.vue
+++ src/components/ClassSchedule/TimeTableCell.vue
@@ -2,9 +2,14 @@
   <div>
     <div v-if="lecture==null">
       <v-card-actions v-if="is_edit && can_register.length >= 1">
-          <div v-if="$store.state.can_register_lectures.forEach(function(lecture){lecture.compulsory==='必修'});">
-        <v-btn @click.stop="dialog = true">授業を登録する</v-btn>
+        <div v-if="hanteikun(can_register)">
+          <v-btn color="error" @click.stop="dialog = true">授業を登録する</v-btn>
+        </div>
+        <div v-else>
+          <v-btn @click.stop="dialog = true">授業を登録する</v-btn>
+        </div>
       </v-card-actions>
+      {{can_register.forEach(function(lecther){lecther.compulsory})}}
     </div>
 
     <div v-else>
@@ -22,7 +27,7 @@
         >授業を取り消す</v-btn>
       </v-card-actions>
     </div>
-    
+
     <!--ダイアログ-->
     <v-dialog v-model="dialog">
       <v-card>
@@ -30,20 +35,42 @@
         <v-container>
           <v-row>
             <v-col v-for="c in can_register" cols="3">
-              <v-card>
-                {{c.name}}
-                <br>
-                {{c.classification}}
-                <br>
-                {{c.compulsory}}
-                <br>
-                {{c.teacher_name1}}
-                <template v-if="c.teacher_name2!=='null'">,他</template>
-                <v-card-actions>
-                  <v-btn v-on:click="$store.commit('push_registered_lecture', c); dialog = false">登録</v-btn>
-                  <v-btn :href="c.syllabus" target="_blank">シラバス</v-btn>
-                </v-card-actions>
-              </v-card>
+              <div v-if="'必修'=== c.compulsory ||'選択必修' === c.compulsory ">
+                <v-card color="orange">
+                  {{c.name}}
+                  <br>
+                  {{c.classification}}
+                  <br>
+                  {{c.compulsory}}
+                  <br>
+                  {{c.teacher_name1}}
+                  <template v-if="c.teacher_name2!=='null'">,他</template>
+                  <v-card-actions>
+                    <v-btn
+                      v-on:click="$store.commit('push_registered_lecture', c); dialog = false"
+                    >登録</v-btn>
+                    <v-btn :href="c.syllabus" target="_blank">シラバス</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </div>
+              <div v-else>
+                <v-card>
+                  {{c.name}}
+                  <br>
+                  {{c.classification}}
+                  <br>
+                  {{c.compulsory}}
+                  <br>
+                  {{c.teacher_name1}}
+                  <template v-if="c.teacher_name2!=='null'">,他</template>
+                  <v-card-actions>
+                    <v-btn
+                      v-on:click="$store.commit('push_registered_lecture', c); dialog = false"
+                    >登録</v-btn>
+                    <v-btn :href="c.syllabus" target="_blank">シラバス</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </div>
             </v-col>
           </v-row>
           <!--<template v-for="c in can_register">-->
@@ -73,6 +100,20 @@ export default {
     return {
       dialog: false
     };
+  },
+  methods: {
+    hanteikun(h) {
+      if (h.length <= 0) {
+        return false;
+      }
+      let test = false;
+      h.forEach(function(lec) {
+        if (lec.compulsory === "必修") {
+          test = true;
+        }
+      });
+      return test;
+    }
   },
   props: ["lecture", "is_edit", "can_register", "day", "time"]
 };
