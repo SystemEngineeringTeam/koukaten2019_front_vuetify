9407973b234c58a3d5c9775d6c540bbaf33142fb
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2019-09-27 00:43:03 +0900

    ログインフォームを作り変えた

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,25 +1,55 @@
 <template>
-  <div>
-    <v-alert type="warning" v-if="show_alert">学席番号かパスワードが間違っています</v-alert>
+  <v-app>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+          <v-alert type="warning" v-if="show_alert">学席番号かパスワードが間違っています</v-alert>
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>ログイン</v-toolbar-title>
+                <div class="flex-grow-1"></div>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form" v-model="valid">
+                  <v-text-field style="width: 80%; margin:5% auto" v-model="ID" :counter="6" :rules="id_rules" label="学籍番号(例 k19000)" required></v-text-field>
 
-    <v-form ref="form" v-model="valid">
-      <v-text-field v-model="ID" :counter="6" :rules="id_rules" label="学籍番号(例 k19000)" required></v-text-field>
+                  <v-text-field
+                    style="width: 80%; margin:5% auto"
+                    v-model="password"
+                    :append-icon="show_pass ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show_pass ? 'text' : 'password'"
+                    :rules="pass_rules"
+                    label="パスワード"
+                    hint="半角アルファベットで8文字以上、小文字大文字数字をそれぞれ1文字以上含む"
+                    counter
+                    @click:append="show_pass = !show_pass"
+                  ></v-text-field>
 
-      <v-text-field
-        v-model="password"
-        :append-icon="show_pass ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="show_pass ? 'text' : 'password'"
-        :rules="pass_rules"
-        label="パスワード"
-        hint="半角アルファベットで8文字以上、小文字大文字数字をそれぞれ1文字以上含む"
-        counter
-        @click:append="show_pass = !show_pass"
-      ></v-text-field>
-
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">ログインする</v-btn>
-      <div v-if="Screen_transition($store.state.user.logined)"></div>
-    </v-form>
-  </div>
+                  <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">ログインする</v-btn>
+                  <div v-if="Screen_transition($store.state.user.logined)"></div>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -68,5 +98,14 @@ export default {
   }
 };
 </script>
+<!--<template>
+  <div>
+
+
+
+  </div>
+</template>
+
+
 
 <style scoped></style>
