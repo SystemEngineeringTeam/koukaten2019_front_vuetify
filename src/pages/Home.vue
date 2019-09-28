adbbfa541b7f5b770621f3d2eac4b85947d7cff0
Sken <k18056kk@aitech.ac.jp>
2019-09-28 14:40:18 +0900

    ホームのデザインの変更

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,10 +12,18 @@
             <!--</router-link>-->
             <!--</v-card>-->
             <p style="padding-top:100px; padding-bottom:100px;">
-              最初にユーザー登録、右上の表示が変わったらエディタページに移動して授業を登録、<br />再読み込みなんかをしてログイン状態じゃなくなったらログインページからログインし直してね
+              最初にユーザー登録、右上の表示が変わったらエディタページに移動して授業を登録、
+              <br />再読み込みなんかをしてログイン状態じゃなくなったらログインページからログインし直してね
             </p>
             <router-link to="/Initial_Registration">
-              <v-btn class="mt-12">ユーザー登録</v-btn>
+              <div class="my-12">
+                <v-btn rounded block x-large>ユーザー登録</v-btn>
+              </div>
+            </router-link>
+            <router-link to="/Login">
+              <div class="my-12">
+                <v-btn rounded block x-large color="#F99F48">ログイン</v-btn>
+              </div>
             </router-link>
           </v-col>
           <!--<v-col cols="6">-->
